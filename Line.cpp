6bafc64f61181421e0279536315d7e439906b1e6
Abanob Essam <abanobessam19@gmail.com>
2025-05-22 15:12:54 +0300

    Fix: fixing the Bresenham line algorithm

--- Line.cpp
+++ Line.cpp
@@ -1,7 +1,3 @@
-//
-// Created by PC on 5/22/2025.
-//
-
 void Parametric_line(HDC hdc,vector<Point>p,COLORREF c){
     int alpha1=p[1].x-p[0].x;
     int alpha2=p[1].y-p[0].y;
@@ -13,100 +9,51 @@ void Parametric_line(HDC hdc,vector<Point>p,COLORREF c){
     }
 }
 void Bresenham_Line(HDC hdc,vector<Point>p,COLORREF c){
-    int dx=p[1].x-p[0].x;
-    int dy=p[1].y-p[0].y;
-    double m=(double)dy/dx;
-    if(m>0&&m<=1){
-        if(p[0].x>p[1].x){
-            swap(p[0].x,p[1].x);
-            swap(p[0].y,p[1].y);
-            int dx=p[1].x-p[0].x;
-            int dy=p[1].y-p[0].y;
+    auto [x1, y1] = p[0];
+    auto [x2, y2] = p[1];
+    int dx = abs(x2 - x1), dy = abs(y2 - y1);
+    if (dx >= dy) {
+        if (x1 > x2) {
+            swap(x1, x2);
+            swap(y1, y2);
         }
-        int d=dx-2*dy;
-        int d1=-2*dy;
-        int d2=2*(dx-dy);
-        int x=p[0].x;
-        int y=p[0].y;
-        SetPixel(hdc,x,y,c);
-        while(x<p[1].x){
-            if(d>0){
-                d+=d1;
-            }else{
-                d+=d2;
-                y++;
+        int sign = (y2 > y1) ? 1 : -1;
+        int d = dx  - 2 * dy;
+        int d1 = 2 * dx - 2 * dy;
+        int d2 = -2 * dy;
+        int x = x1, y = y1;
+        SetPixel(hdc, x, y, c);
+        while (x < x2) {
+            if (d <= 0) {
+                d += d1;
+                y += sign;
             }
+            else
+                d += d2;
             x++;
-            SetPixel(hdc,x,y,c);
+            SetPixel(hdc, x, y, c);
         }
-    }else if(m>1){
-        if(p[0].y>p[1].y){
-            swap(p[0].x,p[1].x);
-            swap(p[0].y,p[1].y);
-            int dx=p[1].x-p[0].x;
-            int dy=p[1].y-p[0].y;
+    }
+    else {
+        if (y1 > y2) {
+            swap(x1, x2);
+            swap(y1, y2);
         }
-        int d=dy-2*dx;
-        int d1=-2*dx;
-        int d2=2*(dy-dx);
-        int x=p[0].x;
-        int y=p[0].y;
-        SetPixel(hdc,x,y,c);
-        while(y<p[1].y){
-            if(d>0){
-                d+=d1;
-            }else{
-                d+=d2;
-                x++;
+        int sign = (x2 > x1) ? 1 : -1;
+        int d = 2 * dx - dy;
+        int d1 = 2 * dx - 2 * dy;
+        int d2 = 2 * dx;
+        int x = x1, y = y1;
+        SetPixel(hdc, x, y, c);
+        while (y < y2) {
+            if (d >= 0) {
+                d += d1;
+                x += sign;
             }
+            else
+                d += d2;
             y++;
-            SetPixel(hdc,x,y,c);
-        }
-    }else if(m<0 && m>-1){
-        if(p[0].x>p[1].x){
-            swap(p[0].x,p[1].x);
-            swap(p[0].y,p[1].y);
-            int dx=p[1].x-p[0].x;
-            int dy=p[1].y-p[0].y;
-        }
-        int d=dx+2*dy;
-        int d1=2*dy;
-        int d2=2*(dx+dy);
-        int x=p[0].x;
-        int y=p[0].y;
-        SetPixel(hdc,x,y,c);
-        while(x<p[1].x){
-            if(d>0){
-                d+=d1;
-            }else{
-                d+=d2;
-                y--;
-            }
-            x++;
-            SetPixel(hdc,x,y,c);
-        }
-    }else{
-        if(p[0].y>p[1].y){
-            swap(p[0].x,p[1].x);
-            swap(p[0].y,p[1].y);
-            int dx=p[1].x-p[0].x;
-            int dy=p[1].y-p[0].y;
-        }
-        int d=dy+2*dx;
-        int d1=2*dx;
-        int d2=2*(dy+dx);
-        int x=p[0].x;
-        int y=p[0].y;
-        SetPixel(hdc,x,y,c);
-        while(y<p[1].y){
-            if(d>0){
-                d+=d1;
-            }else{
-                d+=d2;
-                x--;
-            }
-            y++;
-            SetPixel(hdc,x,y,c);
+            SetPixel(hdc, x, y, c);
         }
     }
 }
